7a5798f1a3fb4982b140849695ec7c4b538c927c
Johnathan Weisner <joweisner@gmail.com>
2016-07-31 15:57:47 -0400

    Inherit from Event system and add game states

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,51 @@
 const Board = require('./Board.js')
+const EventSystem = require('./EventSystem.js')
+const START = 'start';
+const BOUNDS_ERROR = 'bounds_error';
+const GAME_OVER = 'game_over';
 
-class Game {
+class Game extends EventSystem {
     constructor() {
+        super();
         this.board = new Board();
+        this.state = START;
+    }
+
+    onCellSelect({row, col}) {
+        if (this.state === GAME_OVER) return;
+        if (!this.board.inBounds(row, col)) {
+            this.trigger(BOUNDS_ERROR, row, col);
+            return;
+        }
+        let cellSelected = this.board.cells[row][col];
+        if (cellSelected.mine) {
+            this.board.select(row, col);
+            this.state = GAME_OVER;
+            this.trigger(GAME_OVER);
+            return;
+        } else {
+            this.board.select(row, col);
+        }
     }
 }
+
+let game = new Game();
+
+game.bind(BOUNDS_ERROR, (row, col) => {
+    console.log('Error out of bounds', row, col);
+});
+
+game.bind(GAME_OVER, (row, col) => {
+    console.log('The game is over.');
+});
+
+game.onCellSelect({row: -1, col: 2});
+game.onCellSelect({row: 1, col: 2});
+game.onCellSelect({row: 1, col: 3});
+game.onCellSelect({row: 1, col: 4});
+
+console.log(game.board.cells.map((row) => {
+    return row.map((cell) => {
+        return !cell.visible ? '[ ]' : (cell.mine ? '[X]' : '[' + cell.nearCount + ']');
+    });
+}));
